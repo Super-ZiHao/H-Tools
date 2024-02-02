d763ed5f2d06bf28fffaae57d5c79b7c84dc36b3
v_hzihhuang <v_hzihhuang@tencent.com>
2024-02-02 23:09:39 +0800

    perf: 优化

--- src/pages/base64Img.vue
+++ src/pages/base64Img.vue
@@ -1,11 +1,12 @@
 <script lang='ts' setup>
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { ref, computed } from 'vue';
 import isValidVariableName from '@/utils/isValidVariableName.ts'
 import getNestedValues from '@/utils/getNestedValues.ts'
 import downloadImgs from '@/utils/downloadImgs.ts'
+import isBase64Image from '@/utils/isBase64Image.ts'
 import jsBeautify from 'js-beautify';
-const textareaRef = ref<HTMLTextAreaElement>();
-const inputRef = ref<HTMLInputElement>();
+const textareaVlue = ref<string>();
+const inputVlue = ref<string>();
 const isTextareaError = ref(false);
 const isInputError = ref(false);
 
@@ -31,30 +32,21 @@ const inputCallback = (e: any) => {
   if (a.length < 2) return isValidVariableName(value) ? (isInputError.value = false) : (isInputError.value = true);
   a.some(i => !isValidVariableName(i)) ? (isInputError.value = true) : (isInputError.value = false);
 }
-onMounted(() => {
-  textareaRef.value?.addEventListener('input', textareaCallback);
-  inputRef.value?.addEventListener('input', inputCallback);
-});
-onBeforeUnmount(() => {
-  textareaRef.value?.removeEventListener('input', textareaCallback);
-  inputRef.value?.removeEventListener('input', inputCallback);
-});
 
 /** 格式化 textarea 值 */
 const formatJson = () => {
-  if (!textareaRef.value) return;
-  const v = textareaRef.value?.value;
-  textareaRef.value.value = jsBeautify(v, { indent_size: 4, space_in_empty_paren: true });
+  if (!textareaVlue.value) return;
+  textareaVlue.value = jsBeautify(textareaVlue.value, { indent_size: 4, space_in_empty_paren: true });
 }
 
 /** 下载图片 */
 const handlerDownload = () => {
-  if (!textareaRef.value) return;
-  if (textareaRef.value.value === '') return alert('您还未输入base64数组，请先输入。');
+  if (!textareaVlue.value) return;
+  if (textareaVlue.value === '') return alert('您还未输入base64数组，请先输入。');
   if (isTextareaError.value || isInputError.value) return alert('您输入的格式有误，请重新输入');
 
-  const textareaValue = textareaRef.value?.value ?? '';
-  const inputValue = inputRef.value?.value ?? '';
+  const textareaValue = textareaVlue.value ?? '';
+  const inputValue = inputVlue.value ?? '';
   const arr: any = JSON.parse(textareaValue);
 
   const inputArr = inputValue.split('.').map(i => i.trim());
@@ -70,18 +62,51 @@ const handlerDownload = () => {
     }
   }
 }
+
+
+// 当前可下载图片
+const availableImgLength = computed(() => {
+  if (!textareaVlue.value) return 0;
+  if (textareaVlue.value === '') return 0;
+  if (isTextareaError.value || isInputError.value) return 0;
+
+  const textareaValue = textareaVlue.value ?? '';
+  const inputValue = inputVlue.value ?? '';
+  try {
+    const arr: any = JSON.parse(textareaValue);
+    const inputArr = inputValue.split('.').map(i => i.trim());
+    const nestedValuesArr = arr.map((i: any) => getNestedValues(i, inputArr))
+    return (inputValue === '' ? arr : nestedValuesArr).filter((i: string) => isBase64Image(i)).length;
+  } catch(e) {
+
+  }
+
+})
 </script>
 
 <template>
   <div class=" text-2xl mb-6">批量下载 base64 图</div>
   <div class="shadow-2xl border rounded-xl w-full h-full max-w-[1280px] p-4 flex flex-col justify-between">
-    <textarea :class="`h-[calc(100%-60px)] w-full border resize-none p-2 rounded-md border-cyan-400 ${isTextareaError ? 'outline-red-600 outline-2 outline outline-offset-[-2px]' : ''}`" placeholder="例: [{ info: { base64: 'data:image/png;base64,iVBORASDASC' } }, { info: { base64: 'data:image/png;base64,iVBORASDASC2' } }]" ref="textareaRef"></textarea>
+    <textarea
+      :class="`h-[calc(100%-60px)] w-full border resize-none p-2 rounded-md border-cyan-400 ${isTextareaError ? 'outline-red-600 outline-2 outline outline-offset-[-2px]' : ''}`"
+      placeholder="例: [{ info: { base64: 'data:image/png;base64,iVBORASDASC' } }, { info: { base64: 'data:image/png;base64,iVBORASDASC2' } }]"
+      v-model="textareaVlue"
+      ref="textareaRef"
+      @input="textareaCallback"
+    ></textarea>
     <div class="w-full flex justify-between items-center">
       <div class="flex items-center gap-4 h-full">
         图片层级:
-        <input :class="`border border-cyan-400 h-full rounded-md px-2 ${isInputError ? 'outline-red-600 outline-2 outline outline-offset-[-2px]' : ''}`" type="text" placeholder="例: info.base64" alt="" ref="inputRef">
+        <input
+          :class="`border border-cyan-400 h-full rounded-md px-2 ${isInputError ? 'outline-red-600 outline-2 outline outline-offset-[-2px]' : ''}`"
+          type="text"
+          placeholder="例: info.base64"
+          ref="inputRef"
+          v-model="inputVlue"
+          @input="inputCallback"
+        >
 
-        {{ isTextareaError ? '请输入正确的 JSON 格式' : `当前数可下载图片数量: ${JSON.parse(textareaRef?.value ?? '[]').length}` }}
+        {{ isTextareaError ? '请输入正确的 JSON 格式' : `当前数可下载图片数量: ${availableImgLength}` }}
       </div>
       <div class="flex items-center gap-4">
         <button class="py-2 px-6 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 transition-all text-white rounded-md" @click="formatJson">格式化</button>
--- src/utils/downloadImgs.ts
+++ src/utils/downloadImgs.ts
@@ -1,4 +1,3 @@
-
 function groupArrayIntoChunks(arr: any[], chunkSize: number) {
   const chunks = [];
   for (let i = 0; i < arr.length; i += chunkSize) {
@@ -7,41 +6,32 @@ function groupArrayIntoChunks(arr: any[], chunkSize: number) {
   return chunks;
 }
 
-/** 批量下载 */
-function downloadImgs(base64: string[]) {
+/**
+ * @description 批量下载 base64 图片
+ * @param base64 图片地址
+ */
+function downloadBase64Imgs(base64: string[]) {
   let downloadedImages = 1;
   let currentChunkIndex = 0;
   const chunkSize = 5; // 每5个图片进行一次下载
-
-  console.log("downloadImgs",base64)
   function downloadImage(base64Image: string) {
     // 创建一个a标签用于下载
     let a = document.createElement('a');
-    console.log(base64Image, '12312312')
     a.href = base64Image;
     a.download = `image_${downloadedImages}.png`; // 设置下载的文件名
     document.body.appendChild(a); // 添加a标签到body中
     a.click(); // 模拟点击下载
     document.body.removeChild(a); // 移除a标签
     downloadedImages++; // 增加下载图片的数量
-    
-    // 检查是否达到了下载限制
-    if (downloadedImages >= chunkSize) {
-      // 达到了下载限制，可以在这里进行处理，比如提示用户
-      console.log('已达下载限制，无法再下载更多图片。');
-    }
   }
-
-  const newBase64 = groupArrayIntoChunks(base64, chunkSize)
-
-
+  const newBase64 = groupArrayIntoChunks(base64, chunkSize);
   const timer = setInterval(() => {
     newBase64[currentChunkIndex].forEach(downloadImage);
     currentChunkIndex += 1;
     if (currentChunkIndex > newBase64.length - 1) {
       clearInterval(timer);
     }
-  }, 1000)
+  }, 1000);
 }
 
-export default downloadImgs;
+export default downloadBase64Imgs;
