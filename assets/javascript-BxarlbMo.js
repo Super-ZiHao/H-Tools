c9fd4bfe84635f68ca286687c59f5e427f90e14e
v_hzihhuang <v_hzihhuang@tencent.com>
2024-03-12 17:11:58 +0800

    Updates

---
+++ assets/javascript-BxarlbMo.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-B3RKT0P3.js";import"./index.vuevuetypescriptsetuptruelang-YKIWqUl4.js";import"./index-BuCJjjY_.js";import"./el-message-PnYEA2aw.js";import"./el-button-qZUMnpEi.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.46.0(21007360cad28648bdf46282a2592cb47c3a7a6f)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var n=t,c={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{n as conf,c as language};
